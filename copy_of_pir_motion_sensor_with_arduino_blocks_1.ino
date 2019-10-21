e3921b4b90a70ea531ef89bbf5bf3c27df15e6cf
Manveersingh3879 <53978767+Manveersingh3879@users.noreply.github.com>
2019-10-21 22:22:32 +0530

    Add files via upload

---
+++ copy_of_pir_motion_sensor_with_arduino_blocks_1.ino
@@ -0,0 +1,29 @@
+int buttonState = 0;
+
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(4,INPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  buttonState = digitalRead(2);
+  if (buttonState == HIGH) {
+    digitalWrite(13, HIGH);
+    delay(10000); 
+  } else {
+    digitalWrite(13, LOW);
+  }
+  
+   buttonState = digitalRead(4);
+  if (buttonState == HIGH) {
+    digitalWrite(12, HIGH);
+    delay(20000);
+  } else
+     {
+    digitalWrite(12, LOW);
+  }
+}
